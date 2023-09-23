26840f3fc3268722f31a07b687f7cce8f25d9b63
Chrystian Barros <chrystian.barros@hotmail.com>
2023-09-23 00:56:27 -0300

    working with variables

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//In this program file, I'm exposing some concepts that I've learned during Microsoft Learn sessions.
+//variables and its types
+string myName = "Chrystian";
+char nameInitial = 'c';
+int myAge = 22;
+decimal howTallAmI = 1.9M;
+bool lastInfo = false;
+
+//about var: this variable type indicates to the compiler that its type is implict in the value.
+var thisIsAString = "This is a String!";
+
+Console.Write(thisIsAString);
