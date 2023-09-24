bf648d4d31adaa08dd38ade25052c02e38df0e90
Chrystian Barros <chrystian.barros@hotmail.com>
2023-09-24 20:44:59 -0300

    Basic numbers operations

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿//In this program file, I'm exposing some concepts that I've learned during Microsoft Learn sessions.
 //variables and its types
+using System.Globalization;
+
 Console.WriteLine("\tSession: My first variables and its types.");
 
 string myName = "Chrystian";
@@ -43,3 +45,37 @@ string greeting = "\nHello, nice to meet you!";
 myName = $"My name is {name}. What's your name?";
 
 Console.WriteLine(@$"{greeting} {myName}");
+
+//Peforming basic numbers operations
+Console.WriteLine("\n\n\tSession: Perform basic numbers operations");
+
+int firstNumber = 1;
+int secondNumber = 2;
+Console.WriteLine(@$"In this output, I'm adding a first number ({firstNumber}) to a second number ({secondNumber}). The result is: {firstNumber + secondNumber}.");
+
+Console.WriteLine("In the next lines, I'm performing different math operations with the same two numbers: 5 and 2.");
+int sum = 5 + 2;
+int difference = 5 - 2;
+int product = 5 * 2;
+int quotient = 5 / 2;
+decimal decimalQuotient = 5m / 2;
+
+Console.Write($"Sum: {sum}\t");
+Console.Write($"Difference: {difference}\t");
+Console.Write($"Product: {product}\t");
+Console.Write($"Quotient: {quotient}\t");
+Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+
+Console.WriteLine("\nIn the next lines, I'm using increment operators to increase the value of the variable \"varNumber\".");
+
+int varNumber = 0;
+Console.Write($"\"varNumber\" variable first value: {varNumber}.\n");
+Console.Write($"Using the \"varNumber = varNumber + 1\" operator. New value: {varNumber = varNumber + 1}.\n");
+Console.Write($"Using the \"varNumber += 5\" operator (Attention: the last value of the variable was {varNumber}). New value: {varNumber += 5}.\n");
+Console.WriteLine($"Using the \"++varNumber\" operator (Attention: the last value of the variable was {varNumber}). New value: {++varNumber}.");
+
+Console.WriteLine($"\nIn the next line, I'm converting a temperature in Farenheit to Celsius.");
+
+int tempFarenheit = 94;
+decimal tempCelsius = (tempFarenheit - 32) * (5m / 9);
+Console.Write($"Temperature in Farenheit: {tempFarenheit}.\tTemperature in Celsius: {tempCelsius}.");
